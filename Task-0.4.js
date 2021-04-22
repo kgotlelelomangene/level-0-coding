d317c6e770d7e1343be5238c01c7b8367d58dd9b
kgotlelelomangene <kgotlelelomangene@gmail.com>
2021-04-22 16:44:43 +0200

    task 0.4 commit

---
+++ Task-0.4.js
@@ -0,0 +1,14 @@
+//Task 0.4
+console.log("\nTask 0.4 ");
+
+function evenOrOdd(int){
+    if(int%2 == 0 ){
+        console.log("Even");
+    }
+    else{
+        console.log("Odd");
+    }
+        
+}
+
+evenOrOdd(12);
