f5a024f7ba985214a47ed6c512b25c068edae5b7
kgotlelelomangene <kgotlelelomangene@gmail.com>
2021-04-29 14:10:45 +0200

    task 0.8 changes commit

--- Task-0.8.js
+++ Task-0.8.js
@@ -4,14 +4,20 @@ function hoursAndMinutes(num){
     var hours = Math.floor(num/60);
     var minutes = Math.floor(num%60);
 
-    if(hours <= 1 && minutes <= 1){
+    if(hours == 1 && minutes == 1){
         console.log(hours + " hour, " + minutes + " minute");
     }
-    else if(hours <= 1 && minutes > 1){
+    else if(hours == 1 && minutes > 1){
         console.log(hours + " hour, " + minutes + " minutes");
     }
-    else if(hours > 1 && minutes <= 1){
+    else if(hours > 1 && minutes == 1){
         console.log(hours + " hours, " + minutes + " minute");
+    }
+    else if(hours < 1 && minutes == 1){
+        console.log(hours + " hours, " + minutes + " minute");
+    }
+    else if(hours == 1 && minutes < 1){
+        console.log(hours + " hour, " + minutes + " minutes");
     }
     else {
         console.log(hours + " hours, " + minutes + " minutes");
